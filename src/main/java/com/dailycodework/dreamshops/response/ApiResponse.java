49fa00923bef3eb15dced9942cbd2cb3fa7b8f79
Siraj Preena <sirajpreena311@gmail.com>
2024-12-14 23:19:44 +0530

    code adjustments

--- src/main/java/com/dailycodework/dreamshops/response/ApiResponse.java
+++ src/main/java/com/dailycodework/dreamshops/response/ApiResponse.java
@@ -2,9 +2,11 @@ package com.dailycodework.dreamshops.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class ApiResponse {
     private String message;
     private Object data;
